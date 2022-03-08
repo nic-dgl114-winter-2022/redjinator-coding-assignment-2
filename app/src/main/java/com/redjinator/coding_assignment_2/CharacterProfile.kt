4f331792bafbca34de078ff98f46e77a713ac195
Redjinator <71242881+Redjinator@users.noreply.github.com>
2022-03-07 23:20:41 -0800

    Add: second activity for character profile

---
+++ app/src/main/java/com/redjinator/coding_assignment_2/CharacterProfile.kt
@@ -0,0 +1,11 @@
+package com.redjinator.coding_assignment_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CharacterProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_character_profile)
+    }
+}

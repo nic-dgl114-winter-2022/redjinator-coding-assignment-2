493af6d0d6d85a63b02860d878caad83c5148d01
Redjinator <71242881+Redjinator@users.noreply.github.com>
2022-03-07 22:43:45 -0800

    Fix: recyclerview not displaying cards

--- app/src/main/java/com/redjinator/coding_assignment_2/RecyclerAdapter.kt
+++ app/src/main/java/com/redjinator/coding_assignment_2/RecyclerAdapter.kt
@@ -1,5 +1,6 @@
 package com.redjinator.coding_assignment_2
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,22 +10,30 @@ import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(){
 
-    private var title = arrayOf("Chapter One", "Chapter Two", "Chapter Three", "Chapter Four", "Chapter Five", "Chapter Six")
+    private var titles   = arrayOf("Chapter One", "Chapter Two", "Chapter Three", "Chapter Four", "Chapter Five", "Chapter Six")
     private var details = arrayOf("Details One", "Details Two", "Details Three", "Details Four", "Details Five", "Details Six")
-    private var images = arrayOf(R.drawable.cardback, R.drawable.cardback, R.drawable.cardback, R.drawable.cardback, R.drawable.cardback, R.drawable.cardback)
+    private var images  = arrayOf(R.drawable.cardback, R.drawable.cardback, R.drawable.cardback, R.drawable.cardback, R.drawable.cardback, R.drawable.cardback)
+
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.list_items, parent, false)
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.list_items, parent, false)     // Layout Inflater
         return ViewHolder(v)
     }
 
-    override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return titles.size
     }
+
+
+    override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
+        holder.itemTitle.text = titles[position]
+        holder.itemDetail.text = details[position]
+        holder.itemImage.setImageResource(images[position])
+    }
+
+
 
 
     /*

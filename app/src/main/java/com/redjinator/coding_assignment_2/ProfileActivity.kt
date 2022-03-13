c78e9c6997a5ef022106cbb511cf930b9126e649
Redjinator <71242881+Redjinator@users.noreply.github.com>
2022-03-12 19:31:07 -0800

    Add: bottom nav to profile activity and 2 base fragments

--- app/src/main/java/com/redjinator/coding_assignment_2/ProfileActivity.kt
+++ app/src/main/java/com/redjinator/coding_assignment_2/ProfileActivity.kt
@@ -2,26 +2,47 @@ package com.redjinator.coding_assignment_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.redjinator.coding_assignment_2.fragments.CharacterFragment
+import com.redjinator.coding_assignment_2.fragments.ListFragment
 
 private val TAG = "ProfileActivity.kt"
 
 class ProfileActivity : AppCompatActivity() {
 
+    private val characterFragment = CharacterFragment()
+    private val infoFragment = ListFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        //replaceFragment(characterFragment)
 
         val actionBar = supportActionBar
 
         if(actionBar != null) {
-            actionBar.title = "Avocado"
+            actionBar.title = "Menu test"
             actionBar.setDisplayHomeAsUpEnabled(true)
         }
 
-        // -------------
+        var bottom_navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_profile -> replaceFragment(characterFragment)
+                R.id.ic_main_list -> replaceFragment(infoFragment)
+            }
+            true
+        }
 
+    }
 
-
+    private fun replaceFragment(fragment: Fragment) {
+        if(fragment !=null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.profile_fragment_container, fragment)
+            transaction.commit()
+        }
     }
 }
